e5168d9bf351e882b75c1d32b92c026dff7094aa
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-19 18:10:46 +0200

    using catalog

--- servieres.php
+++ servieres.php
@@ -3,25 +3,12 @@
 $title = "Lac de Servières";
 $meta_description = "Hike and Camp - Page de réservation pour le lac de Servières, Orcival";
 
+include 'catalog-with-keys.php';
+
+$item = $servieres;
+
 include 'header.php';
-
-$titre_item = "Lac de Servières - 1200m";
-$lieu_item = "63210 Orcival";
-$prix_item = 45;
-$note_item = "⭐⭐⭐⭐";
-$image_item = "./Images/lieux/servieres.jpg";
-$description_image_item = "Lac de Servières";
-$description_item = "Le lac de Servières, situé à 1 200 mètres d'altitude dans le Puy-de-Dôme, est un maar volcanique de 15,5
-                hectares et 26 mètres de profondeur, formé par une explosion volcanique due à la rencontre du magma et
-                d'une nappe phréatique. Classé Espace Naturel Sensible, le site abrite des herbiers aquatiques protégés
-                et des vestiges d'anciens burons appelés « tras ». Les activités sont réglementées pour préserver cet
-                environnement fragile : la baignade, le canotage et les feux y sont interdits ; les chiens doivent être
-                tenus en laisse ; le bivouac est autorisé uniquement dans le cadre d'une randonnée itinérante. Une
-                promenade familiale de 2 km permet de faire le tour du lac en environ une heure.";
-$carte_item = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.2630610510255!2d2.8567755266375867!3d45.645538721320676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f70ca41d28628d%3A0xbee4f631141122c4!2sLac%20de%20Servi%C3%A8res!5e0!3m2!1sfr!2sfr!4v1743601567251!5m2!1sfr!2sfr";
-
 include 'item.php';
-
 include 'footer.php';
 
 ?>
