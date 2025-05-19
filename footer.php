40c9758a3542ef379261a2770eaccbb18a2c1663
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-19 16:02:48 +0200

    footer en php

---
+++ footer.php
@@ -0,0 +1,42 @@
+  <!-- footer -->
+    <footer class="container-fluid mt-5 p-5 bg-warning-subtle">
+        <div class="row">
+            <div class="row">
+                <h2 class="fs-1">Contact</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-xl-4">
+                    <h3 class="fs-5">10 Rue des Chevaliers 26000 Valence</h3>
+                    <a href="mailto:hikeandcamp@entreprise.fr" class="link-dark fs-5 link-underline-opacity-0">Mail:
+                        hikeandcamp@entreprise.fr</a>
+                </div>
+                <div class="col-12 col-xl-4">
+                    <div class="d-flex row justify-content-center">
+                        <a href="" class="col-3 col-xl-2" role="button"><img src="./Images/appstore.png" class="img-fluid" alt="logo-appstore" ></a>
+                        <a href="" class="col-3 col-xl-2" role="button"><img src="./Images/playstore.png" class="img-fluid" alt="logo_playstore"></a>
+                    </div>
+                </div>
+
+                <div class="col-12 col-xl-4">
+                    <h2 class="fs-5">Suivez Nous:</h2>
+                    <div class="row">
+                        <a class="col-3 col-xl-2 " id="logo_insta" href="https://www.instagram.com/"><img
+                                class="img-fluid" src="Images/instagram.png" alt="logo instagram"
+                                title="logo instagram"></a>
+                        <a class="col-3 col-xl-2" id="logo_face" href="https://www.facebook.com"><img class="img-fluid"
+                                src="Images/facebook.png" alt="logo facebook" title="logo facebook"></a>
+                        <a class="col-3 col-xl-2" id="logo_twit" href="https://x.com"><img class="img-fluid"
+                                src="Images/twitter.png" alt="logo twitter" title="logo twitter"></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
