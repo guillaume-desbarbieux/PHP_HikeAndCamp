00b7ca6d4a8ec404a8390e4b9f7732bd678d9ab3
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-19 16:02:35 +0200

    header avec titre et meta description dynamique

--- header.php
+++ header.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
 
     <?php
-    $title = "Produit 1";
-    echo    "
+        echo    "
+            <meta name='description' content=$meta_description>
             <title>$title</title>
             ";
     ?>
