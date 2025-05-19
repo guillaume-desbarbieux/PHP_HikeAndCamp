b92ebfc1f7332edcc13bbcfb7f58a2dd19909726
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-19 18:10:00 +0200

    using catalog

--- roybon.php
+++ roybon.php
@@ -3,27 +3,12 @@
 $title = "Cabane De Roybon";
 $meta_description = "Hike and Camp - Page de réservation pour la Cabane de Roybon, Villard de Lans";
 
+include 'catalog-with-keys.php';
+
+$item = $roybon;
+
 include 'header.php';
-
-$titre_item = "Cabane de Roybon - 1450m";
-$lieu_item = "38250 Villard de Lans";
-$prix_item = 50;
-$note_item = "⭐⭐⭐⭐⭐";
-$image_item = "./Images/lieux/roybon.jpeg";
-$description_image_item = "Cabane du Roybon";
-$description_item = "La cabane des Clots, également appelée cabane de Roybon, est un refuge non gardé situé à environ
-                1 450 mètres d'altitude dans le Parc Naturel Régional du Vercors. Accessible uniquement à pied,
-                elle offre une capacité d'accueil d'environ 15 personnes sur des bat-flancs superposés. La
-                cabane dispose de volets aux fenêtres, de tables à l'intérieur et à l'extérieur, ainsi que d'une
-                cheminée fonctionnelle mais pouvant enfumer l'intérieur si le feu n'est pas correctement
-                positionné au fond. Une source d'eau, la fontaine de Roybon, se trouve à proximité immédiate,
-                bien que son débit puisse varier selon la saison. Les toilettes sèches sont généralement bien
-                entretenues. Il est recommandé aux visiteurs d'apporter leur propre bois de chauffage, car la
-                disponibilité sur place peut être limitée.";
-$carte_item = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2818.6133292530353!2d5.584070376612646!3d45.05306796077566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478a9159ac30d5fb%3A0x33c7485dbf361975!2sCabane%20de%20Roybon!5e0!3m2!1sfr!2sfr!4v1743587497155!5m2!1sfr!2sfr";
-
 include 'item.php';
-
 include 'footer.php';
 
 ?>
