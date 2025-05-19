a6b462523d5e392720f960a24176a66d06b45909
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-19 18:01:56 +0200

    Create catalog-with-keys.php

---
+++ catalog-with-keys.php
@@ -0,0 +1,67 @@
+<?php
+$roybon = [
+  "titre"        => "Cabane de Roybon - 1450m",
+  "lieu"         => "38250 Villard de Lans",
+  "prix"         => 5000,
+  "note"        => "⭐⭐⭐⭐⭐",
+  "image"        => [
+                "url"          => "./Images/lieux/roybon.jpeg",
+                "description"  => "Cabane du Roybon",
+                  ],
+  "description"  => "La cabane des Clots, également appelée cabane de Roybon, est un refuge non gardé situé à environ
+                1 450 mètres d'altitude dans le Parc Naturel Régional du Vercors. Accessible uniquement à pied,
+                elle offre une capacité d'accueil d'environ 15 personnes sur des bat-flancs superposés. La
+                cabane dispose de volets aux fenêtres, de tables à l'intérieur et à l'extérieur, ainsi que d'une
+                cheminée fonctionnelle mais pouvant enfumer l'intérieur si le feu n'est pas correctement
+                positionné au fond. Une source d'eau, la fontaine de Roybon, se trouve à proximité immédiate,
+                bien que son débit puisse varier selon la saison. Les toilettes sèches sont généralement bien
+                entretenues. Il est recommandé aux visiteurs d'apporter leur propre bois de chauffage, car la
+                disponibilité sur place peut être limitée.",
+  "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2818.6133292530353!2d5.584070376612646!3d45.05306796077566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478a9159ac30d5fb%3A0x33c7485dbf361975!2sCabane%20de%20Roybon!5e0!3m2!1sfr!2sfr!4v1743587497155!5m2!1sfr!2sfr",
+  ];
+
+
+
+$servieres = [
+  "titre"        => "Lac de Servières - 1200m",
+  "lieu"         => "63210 Orcival",
+  "prix"         => 4500,
+  "note"         => "⭐⭐⭐⭐",
+  "image"        => [
+                "url"          => "./Images/lieux/servieres.jpg",
+                "description"  => "Lac de Servières",
+                  ],
+  "description"  => "Le lac de Servières, situé à 1 200 mètres d'altitude dans le Puy-de-Dôme, est un maar volcanique de 15,5
+                hectares et 26 mètres de profondeur, formé par une explosion volcanique due à la rencontre du magma et
+                d'une nappe phréatique. Classé Espace Naturel Sensible, le site abrite des herbiers aquatiques protégés
+                et des vestiges d'anciens burons appelés « tras ». Les activités sont réglementées pour préserver cet
+                environnement fragile : la baignade, le canotage et les feux y sont interdits ; les chiens doivent être
+                tenus en laisse ; le bivouac est autorisé uniquement dans le cadre d'une randonnée itinérante. Une
+                promenade familiale de 2 km permet de faire le tour du lac en environ une heure.",
+  "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.2630610510255!2d2.8567755266375867!3d45.645538721320676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f70ca41d28628d%3A0xbee4f631141122c4!2sLac%20de%20Servi%C3%A8res!5e0!3m2!1sfr!2sfr!4v1743601567251!5m2!1sfr!2sfr"
+  ];
+
+
+$charlesdegaulle = [
+  "titre"        => "Porte-avion Charles de Gaulle - 12m",
+  "lieu"         => "Actuellement : Détroit de Messine, Méditerrannée",
+  "prix"         => 15000000,
+  "note"         => "⭐⭐⭐⭐⭐⭐⭐",
+  "image"        => [
+                "url"          => "./Images/lieux/porteavion.jpg",
+                "description"  => "Porte-avion Charles de Gaulle",
+                  ],
+  "description"  => "Vivez une expérience unique en passant une nuit à la belle étoile sur le porte-avions Charles de Gaulle !
+                Perché sur le pont d’envol, face à l’immensité de l’océan, ce bivouac hors du commun vous promet une vue
+                imprenable sur l’horizon et un réveil au son des vagues.
+
+                Équipé d’un espace sécurisé pour les campeurs et d’une zone de repos avec accès à des installations
+                militaires, ce lieu de bivouac s’adresse aux aventuriers en quête d’originalité. Le vent marin et le
+                vrombissement lointain des turbines rythmeront votre nuit, tandis que le ciel dégagé offrira un
+                spectacle céleste incomparable. Une occasion unique de mêler nature et technologie dans un cadre
+                exceptionnel !",
+  "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12535.989265270897!2d15.626502169409127!3d38.23316960502553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131452fc94c7b677%3A0x75e0d4c85804c8e6!2sD%C3%A9troit%20de%20Messine!5e0!3m2!1sfr!2sfr!4v1743603018501!5m2!1sfr!2sfr",
+  ];
+
+
+
