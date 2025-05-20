5434b6733ee64060fd465611a9afd6cbc80c83ce
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-20 14:04:52 +0200

    ajout nouveau form

--- item_catalogue.php
+++ item_catalogue.php
@@ -1,26 +1,32 @@
-<div class="row justify-content-center">
-    <div class="col-12 col-xl-5 mt-2">
-        <div class="row m-0 img-fluid border border-dark border-3 rounded-5 p-1">
-            <div class="col-8 d-flex justify-content-center align-items-center ">
-                <div class="ratio ratio-4x3 m-2">
-                    <img class='img-fluid object-fit-cover border border-warning border-3 rounded-2'
-                        src=<?php echo $item["image"]["url"] ?> alt=<?php echo $item["image"]["description"] ?> title=<?php echo $item["titre"] ?>>
-                </div>
-            </div>
-            <div class="col-4 d-flex flex-column align-items-center">
-                <h3 class="row fs-4"><?php echo $item["titre"] ?></h3>
-                <h4 class="row fs-6"><?php echo $item["note"] ?></h4>
-                <h4 class="row fs-6"><?php discountedPrice($item["prix"], $item["discount"]); ?> (TTC)</h4>
-                <h5 class="row fs-6"><?php priceExcludingVAT($item["prix"], $item["discount"]); ?> (HT)</h5>
-                <div class="row d-flex m-1 w-100">
-                    <a href="#formulaire_reservation" class="btn btn-success m-auto" role="button">Réserver</a>
-                </div>
-                <div class="row d-flex m-1 w-100">
-                    <a href="<?php echo $item["page"] ?>" class='btn btn-outline-success m-auto' role='button'>Voir</a>
-
-                </div>
+<div class="col-12 col-xl-5 mt-2">
+    <div class="row m-0 img-fluid border border-dark border-3 rounded-5 p-1">
+        <div class="col-8 d-flex justify-content-center align-items-center ">
+            <div class="ratio ratio-4x3 m-2">
+                <img class='img-fluid object-fit-cover border border-warning border-3 rounded-2'
+                    src=<?php echo $item["image"]["url"] ?> alt=<?php echo $item["image"]["description"] ?> title=<?php echo $item["titre"] ?>>
             </div>
         </div>
-    </div>
+        <div class="col-4 d-flex flex-column align-items-center">
+            <h3 class="row fs-4"><?php echo $item["titre"] ?></h3>
+            <h4 class="row fs-6"><?php echo $item["note"] ?></h4>
+            <h4 class="row fs-6"><?php discountedPrice($item["prix"], $item["discount"]); ?> (TTC)</h4>
+            <h5 class="row fs-6"><?php priceExcludingVAT($item["prix"], $item["discount"]); ?> (HT)</h5>
+            <!-- Bouton accès ancien formulaire de réservation -->
+            <!-- <div class="row d-flex m-1 w-100">
+                    <a href="#formulaire_reservation" class="btn btn-success m-auto" role="button">Réserver</a>
+                </div> -->
+            <div class="row d-flex m-1 w-100">
+                <a href="<?php echo $item["page"] ?>" class='btn btn-outline-success m-auto' role='button'>Voir les détails</a>
+            </div>
+            <form method="post" action="cart.php">
+                <label for="nights">Nombre de nuits :</label>
+                <input type="number" id="nights" name="night" min="1" max="10" required/>
+                <span class="error">*<?php echo $nightErr; ?></span>
+                <div class="row d-flex m-1 w-100">
+                    <input type="submit" class="btn btn-success m-auto" name="submit" value="Ajouter au panier">
+                </div>
+            </form>
 
+        </div>
+    </div>
 </div>
