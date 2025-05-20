5936e1d2f84024f56dfd159f75959694ba0448c5
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-20 15:08:51 +0200

    function cout de transport

--- my-functions.php
+++ my-functions.php
@@ -1,26 +1,55 @@
 <?php
-function formatPrice(int $price) {
-echo ($price/100)."€";
+function formatPrice(int $price)
+{
+    echo ($price / 100) . "€";
 }
 
-function priceExcludingVAT(int $price, int $discount = 0) {
-return formatPrice((100-$discount)*$price/120);
+function priceExcludingVAT(int $price, int $discount = 0)
+{
+    return formatPrice((100 - $discount) * $price / 120);
 }
 
-function priceVAT(int $price, int $discount = 0) {
-return formatPrice((100-$discount)*$price*0.002);
+function priceVAT(int $price, int $discount = 0)
+{
+    return formatPrice((100 - $discount) * $price * 0.002);
 }
 
-function discountedPrice(int $price, int $discount = 0) {
-return formatPrice((100-$discount)*$price/100);
+function discountedPrice(int $price, int $discount = 0)
+{
+    return formatPrice((100 - $discount) * $price / 100);
 }
 
-function test_input ($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 
+function transport_price($transport, $distance)
+{
+    if ($transport == "jet") {
+        $cout = 10000;
+    };
+
+    if ($transport == "taxi") {
+        $cout = 1000;
+    };
+
+    if ($transport == "marche") {
+        $cout = 10;
+    };
+
+    // si moins de 10km, pas de frais de transport. Moins de 100km, prix au km selon transport. Plus de 100km, prix fixe de 150km !
+    if ($distance < 10) {
+        $livraison = 0;
+    } elseif ($distance < 100) {
+        $livraison = $cout * $distance;
+    } else {
+        $livraison = 150 * $cout;
+    };
+    return $livraison;
+}
 
 ?>
