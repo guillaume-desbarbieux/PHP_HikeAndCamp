a06bb64c425a8cd6d232b991a24dfacc7a69932d
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-20 09:08:55 +0200

    template item catalogue

---
+++ item_catalogue.php
@@ -0,0 +1,36 @@
+<div class="row justify-content-center">
+            <div class="col-12 col-xl-5 mt-2">
+                <div class="row m-0 img-fluid border border-dark border-3 rounded-5 p-1">
+                    <div class="col-8 d-flex justify-content-center align-items-center ">
+                        <div class="ratio ratio-4x3 m-2">
+                            <?php
+                            echo    "
+                                    <img class='img-fluid object-fit-cover border border-warning border-3 rounded-2'
+                                    src=$item[image][url] alt=$item[image][description] title=$item[titre]>
+                                    ";
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-4 d-flex flex-column align-items-center">
+                        <?php
+                        echo    "
+                                <h3 class='row fs-4'>$item[titre]</h3>
+                                <h4 class='row fs-6'>$item[note]</h4>
+                                <h4 class='row fs-6'>$item[prix]</h4>
+                                ";
+                        ?>
+                        <div class="row d-flex m-1 w-100">
+                            <a href="#formulaire_reservation" class="btn btn-success m-auto" role="button">Reserver</a>
+                        </div>
+                        <div class="row d-flex m-1 w-100">
+                            <?php
+                            echo    "
+                                    <a href=$item[page] class='btn btn-outline-success m-auto' role='button'>Voir</a>
+                                    ";
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
