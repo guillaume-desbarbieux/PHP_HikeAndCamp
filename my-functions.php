ee80f7693c2e1b8c63d62159ce25d34a2cd6ac42
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-20 14:28:36 +0200

    priceVAT et test_input

--- my-functions.php
+++ my-functions.php
@@ -7,7 +7,20 @@ function priceExcludingVAT(int $price, int $discount = 0) {
 return formatPrice((100-$discount)*$price/120);
 }
 
+function priceVAT(int $price, int $discount = 0) {
+return formatPrice((100-$discount)*$price*0.002);
+}
+
 function discountedPrice(int $price, int $discount = 0) {
 return formatPrice((100-$discount)*$price/100);
 }
+
+function test_input ($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+
+
 ?>
