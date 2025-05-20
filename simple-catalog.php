b644cb25296d5b3b3d95a7957e58dd5680e735df
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-20 11:48:03 +0200

    fix bug affichage

--- simple-catalog.php
+++ simple-catalog.php
@@ -3,7 +3,9 @@ $products = ["roybon","servieres","charlesdegaulle"];
 
 sort($products);
 
-echo "$products[0]";
-echo "$products[count($products)-1]";
+echo $products[0];
+echo $products[count($products)-1];
+
+
 
 ?>
