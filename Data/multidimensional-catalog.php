b042b3e5798134597be6ee0cd161db585268321b
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-05-22 14:18:23 +0200

    ajout nouveaux produits au catalogue

--- Data/multidimensional-catalog.php
+++ Data/multidimensional-catalog.php
@@ -55,17 +55,98 @@ $products = [
   ],
 
 
-  "charlesdegaulle" => [
+  "charlesdegaulle2" => [
     "distance"    =>750,
-    "name"        =>"charlesdegaulle",
+    "name"        =>"charlesdegaulle2",
     "title"        => "Porte-avion Charles de Gaulle",
     "meta_description"  => "Hike and Camp - Page de réservation pour le porte-avion Charles de Gaulle, détroit de Messine - Mer Méditerrannée",
     "titre"        => "Porte-avion Charles de Gaulle - 12m",
     "lieu"         => "Actuellement : Détroit de Messine, Méditerrannée",
-    "page"         => "charlesdegaulle",
-    "prix"         => 15000000,
+    "page"         => "charlesdegaulle2",
+    "prix"         => 1575,
     "discount"    => 25,
-    "note"         => "⭐⭐⭐⭐⭐⭐⭐",
+    "note"         => "⭐",
+    "image"        => [
+      "url"          => "./Images/lieux/porteavion.jpg",
+      "description"  => "Porte-avion Charles de Gaulle",
+    ],
+    "description"  => "Vivez une expérience unique en passant une nuit à la belle étoile sur le porte-avions Charles de Gaulle !
+                Perché sur le pont d’envol, face à l’immensité de l’océan, ce bivouac hors du commun vous promet une vue
+                imprenable sur l’horizon et un réveil au son des vagues.
+
+                Équipé d’un espace sécurisé pour les campeurs et d’une zone de repos avec accès à des installations
+                militaires, ce lieu de bivouac s’adresse aux aventuriers en quête d’originalité. Le vent marin et le
+                vrombissement lointain des turbines rythmeront votre nuit, tandis que le ciel dégagé offrira un
+                spectacle céleste incomparable. Une occasion unique de mêler nature et technologie dans un cadre
+                exceptionnel !",
+    "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12535.989265270897!2d15.626502169409127!3d38.23316960502553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131452fc94c7b677%3A0x75e0d4c85804c8e6!2sD%C3%A9troit%20de%20Messine!5e0!3m2!1sfr!2sfr!4v1743603018501!5m2!1sfr!2sfr",
+  ],
+
+  "charlesdegaulle3" => [
+    "distance"    =>750,
+    "name"        =>"charlesdegaulle3",
+    "title"        => "Porte-avion Charles de Gaulle",
+    "meta_description"  => "Hike and Camp - Page de réservation pour le porte-avion Charles de Gaulle, détroit de Messine - Mer Méditerrannée",
+    "titre"        => "Porte-avion Charles de Gaulle - 12m",
+    "lieu"         => "Actuellement : Détroit de Messine, Méditerrannée",
+    "page"         => "charlesdegaulle3",
+    "prix"         => 14000,
+    "discount"    => 25,
+    "note"         => "⭐⭐",
+    "image"        => [
+      "url"          => "./Images/lieux/porteavion.jpg",
+      "description"  => "Porte-avion Charles de Gaulle",
+    ],
+    "description"  => "Vivez une expérience unique en passant une nuit à la belle étoile sur le porte-avions Charles de Gaulle !
+                Perché sur le pont d’envol, face à l’immensité de l’océan, ce bivouac hors du commun vous promet une vue
+                imprenable sur l’horizon et un réveil au son des vagues.
+
+                Équipé d’un espace sécurisé pour les campeurs et d’une zone de repos avec accès à des installations
+                militaires, ce lieu de bivouac s’adresse aux aventuriers en quête d’originalité. Le vent marin et le
+                vrombissement lointain des turbines rythmeront votre nuit, tandis que le ciel dégagé offrira un
+                spectacle céleste incomparable. Une occasion unique de mêler nature et technologie dans un cadre
+                exceptionnel !",
+    "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12535.989265270897!2d15.626502169409127!3d38.23316960502553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131452fc94c7b677%3A0x75e0d4c85804c8e6!2sD%C3%A9troit%20de%20Messine!5e0!3m2!1sfr!2sfr!4v1743603018501!5m2!1sfr!2sfr",
+  ],
+
+  "charlesdegaulle4" => [
+    "distance"    =>750,
+    "name"        =>"charlesdegaulle4",
+    "title"        => "Porte-avion Charles de Gaulle",
+    "meta_description"  => "Hike and Camp - Page de réservation pour le porte-avion Charles de Gaulle, détroit de Messine - Mer Méditerrannée",
+    "titre"        => "Porte-avion Charles de Gaulle - 12m",
+    "lieu"         => "Actuellement : Détroit de Messine, Méditerrannée",
+    "page"         => "charlesdegaulle4",
+    "prix"         => 150000,
+    "discount"    => 25,
+    "note"         => "⭐⭐⭐",
+    "image"        => [
+      "url"          => "./Images/lieux/porteavion.jpg",
+      "description"  => "Porte-avion Charles de Gaulle",
+    ],
+    "description"  => "Vivez une expérience unique en passant une nuit à la belle étoile sur le porte-avions Charles de Gaulle !
+                Perché sur le pont d’envol, face à l’immensité de l’océan, ce bivouac hors du commun vous promet une vue
+                imprenable sur l’horizon et un réveil au son des vagues.
+
+                Équipé d’un espace sécurisé pour les campeurs et d’une zone de repos avec accès à des installations
+                militaires, ce lieu de bivouac s’adresse aux aventuriers en quête d’originalité. Le vent marin et le
+                vrombissement lointain des turbines rythmeront votre nuit, tandis que le ciel dégagé offrira un
+                spectacle céleste incomparable. Une occasion unique de mêler nature et technologie dans un cadre
+                exceptionnel !",
+    "carte"        => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12535.989265270897!2d15.626502169409127!3d38.23316960502553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131452fc94c7b677%3A0x75e0d4c85804c8e6!2sD%C3%A9troit%20de%20Messine!5e0!3m2!1sfr!2sfr!4v1743603018501!5m2!1sfr!2sfr",
+  ],
+
+  "charlesdegaulle5" => [
+    "distance"    =>750,
+    "name"        =>"charlesdegaulle5",
+    "title"        => "Porte-avion Charles de Gaulle",
+    "meta_description"  => "Hike and Camp - Page de réservation pour le porte-avion Charles de Gaulle, détroit de Messine - Mer Méditerrannée",
+    "titre"        => "Porte-avion Charles de Gaulle - 12m",
+    "lieu"         => "Actuellement : Détroit de Messine, Méditerrannée",
+    "page"         => "charlesdegaulle5",
+    "prix"         => 1500,
+    "discount"    => 25,
+    "note"         => "⭐⭐",
     "image"        => [
       "url"          => "./Images/lieux/porteavion.jpg",
       "description"  => "Porte-avion Charles de Gaulle",
